28c7da1482ffb0429d6624a5af84c7b37da474e1
Dev Ttangkong <ttankkeo112@gmail.com>
2024-11-10 18:37:32 +0900

    Update client files to modifying svg files about svg and fade-animation of page and initial

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,15 @@ import "./styles/main";
 import "./styles/font";
 import "./styles/variables";
 import "./styles/templates";
+import "./styles/keyframes";
 
 import "web-touch-ripple";
 
-render(<App />, document.body);
+addEventListener("load", () => render(<App />, document.body));
+addEventListener("load", () => {
+    document.body.style.animation = "page_fade-in 0.5s";
+})
+addEventListener("beforeunload", () => {
+    document.body.style.animation = "page_fade-out 0.5s";
+    document.body.style.animationFillMode = "backwards";
+});
