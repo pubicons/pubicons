c55caa8b435be8e83f3a2610ba08a81caa170cb7
Dev Ttangkong <ttankkeo112@gmail.com>
2024-11-22 20:52:28 +0900

    commit for backup about client

--- client/src/pages/app/Home.tsx
+++ client/src/pages/app/Home.tsx
@@ -1,8 +1,12 @@
+import SearchIcon from "../../assets/svgs/search.svg";
+import MikeIcon from "../../assets/svgs/mike.svg";
 import { Box, Column, Row, Scrollable } from "@web-package/react-widgets";
+import { TouchRipple } from "web-touch-ripple/jsx";
 
 export function HomePage() {
     return (
         <Column paddingAndGap="var(--padding-df)">
+            <Header />
             <Column>
                 <h2>Your History</h2>
                 <span>The icons that is you previously downloaded.</span>
@@ -20,6 +24,43 @@ export function HomePage() {
     )
 }
 
+function Header() {
+    return (
+        <Row align="center" gap="var(--padding-sm)">
+            <HeaderSearchBar />
+            <HeaderSearchVoice />
+        </Row>
+    )
+}
+
+function HeaderSearchBar() {
+    return (
+        <Row
+            width="100%"
+            maxWidth="600px"
+            paddingLeft="var(--padding-df)"
+            backgroundColor="var(--rearground)"
+            borderRadius="1e10px"
+        >
+            <SearchIcon width="18px" style={{fill: "var(--foreground2)"}} />
+            <input placeholder="Enter a name of icons and keywords or alias" style={{
+                width: "100%",
+                padding: "var(--padding-df)"
+            }} />
+        </Row>
+    )
+}
+
+function HeaderSearchVoice() {
+    return ( 
+        <TouchRipple onTap={() => {}}>
+            <Box display="flex" padding="var(--padding-df)" backgroundColor="var(--rearground)" borderRadius="1e10px">
+                <MikeIcon width="20px" height="20px" />
+            </Box>
+        </TouchRipple>
+    )
+}
+
 function Category({title, selected}: {
     title: string;
     selected: boolean;
@@ -27,6 +68,7 @@ function Category({title, selected}: {
     return (
         <Box
             padding="10px 15px"
+            fontSize="14px"
             borderRadius="1e10px"
             backgroundColor={selected ? "var(--rearground)" : "transparent"}
             border="1px solid var(--rearground-border)"
